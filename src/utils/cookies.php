4a605a73006bceb6f969cf2bbff7d398f44b0e39
STaninnat <taninnat.s@gmailcom>
2025-08-28 05:58:16 +0700

    feat: implement cookie and token management functions

---
+++ src/utils/cookies.php
@@ -0,0 +1,34 @@
+<?php
+
+function getAccessTokenFromCookie(): ?string
+{
+    return $_COOKIE['access_token'] ?? null;
+}
+
+function setAccessTokenCookie(string $token, int $expires): void
+{
+    setcookie('access_token', $token, [
+        'expires'  => $expires,
+        'path'     => '/',
+        'secure'   => true,
+        'httponly' => true,
+        'samesite' => 'Strict',
+    ]);
+}
+
+function clearAccessTokenCookie(): void
+{
+    setcookie('access_token', '', [
+        'expires'  => time() - 3600,
+        'path'     => '/',
+        'secure'   => true,
+        'httponly' => true,
+        'samesite' => 'Strict',
+    ]);
+}
+
+
+function getAccessTokenCookie(): ?string
+{
+    return $_COOKIE['access_token'] ?? null;
+}

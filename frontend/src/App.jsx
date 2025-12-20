df0f40c3214b348922b3c0297d78d96937f10711
STaninnat <taninnat.s@gmailcom>
2025-12-21 06:36:45 +0700

    feat: add Lenis for smooth scrolling and custom scrollbar styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
+import { ReactLenis } from 'lenis/react';
 import AppRoutes from './components/AppRoutes';
 
 export function App() {
     return (
-        <BrowserRouter>
-            <Toaster position="bottom-right" />
-            <AppRoutes />
-        </BrowserRouter>
+        <ReactLenis root>
+            <BrowserRouter>
+                <Toaster position="bottom-right" />
+                <AppRoutes />
+            </BrowserRouter>
+        </ReactLenis>
     );
 }
 
